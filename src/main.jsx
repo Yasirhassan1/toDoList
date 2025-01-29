031f1cb2eb9d377d7cead52703cd5e3cbd723d46
Yasirhassan1 <yasuhassan7896@gmail.com>
2025-01-29 09:34:10 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes } from "react-router-dom"; // Import HashRouter and Routes
+import { BrowserRouter} from "react-router-dom"; // Import HashRouter and Routes
 
 import "./index.css";
 import App from "./App.jsx";
@@ -8,9 +8,7 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/toDoList/">
-    <Routes>
     <App />
-    </Routes>
     </BrowserRouter>
   </StrictMode>
 );
